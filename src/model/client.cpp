fdc00219bfbad419ee0f312e1dc544457eefa9f0
KevinJGard <kevingard@ciencias.unam.mx>
2024-09-01 17:12:26 -0600

    - Refactorización del cliente y el servidor agregando clases y métodos para evitar el uso de variables globales y mejorar la estructura y legibilidad. - Agregue hilos para manejar multiples conexiones de clientes al servidor.

--- src/model/client.cpp
+++ src/model/client.cpp
@@ -4,7 +4,71 @@
 #include <cstdlib>
 #include <unistd.h>
 #include <cstring>
+
 using namespace std;
+
+class Client {
+public:
+    Client(int port) : port(port) {
+        setup_client();
+    }
+
+    ~Client() {
+        if (sockfd >= 0) {
+            close(sockfd);
+        }
+    }
+
+    void run() {
+        cout << "Connected to server" << endl;
+        cout << "Enter your identification: ";
+        string identification;
+        getline(cin, identification);
+        send(sockfd, identification.c_str(), identification.length(), 0);
+
+        while (true) {
+            cout << "Enter your message: ";
+            string message;
+            getline(cin, message);
+            if (message == "/exit") {
+                send(sockfd, message.c_str(), message.length(), 0);
+                break;
+            }
+            send(sockfd, message.c_str(), message.length(), 0);
+            char buffer[BUFFER_SIZE] = {0};
+            int n = recv(sockfd, buffer, BUFFER_SIZE - 1, 0);
+            if (n > 0) {
+                buffer[n] = '\0';
+                cout << "Server: " << buffer << endl;
+            } else {
+                cerr << "Error receiving data from server." << endl;
+                break;
+            }
+        }
+    }
+
+private:
+    int port;
+    int sockfd;
+    const int BUFFER_SIZE = 256;
+
+    void setup_client() {
+        sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        if (sockfd == -1) {
+            cout << "Failed to create socket." << endl;
+            exit(EXIT_FAILURE);
+        }
+        sockaddr_in server_addr;
+        server_addr.sin_family = AF_INET;
+        server_addr.sin_addr.s_addr = INADDR_ANY;
+        server_addr.sin_port = htons(port);
+        if (connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+            cout << "Error connecting to server..." << endl;
+            close(sockfd);
+            exit(EXIT_FAILURE);
+        }
+    }
+};
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -17,51 +81,8 @@ int main(int argc, char *argv[]) {
         cerr << "Invalid port number. Port must be between 1 and 65535." << endl;
         exit(EXIT_FAILURE);
     }
+    Client client(port);
+    client.run();
 
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1) {
-        cout << "Failed to create socket." << endl;
-        exit(EXIT_FAILURE);
-    }
-
-    sockaddr_in server_addr;
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(port);
-
-    if (connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
-        cout << "Error connecting to server..." << endl;
-        close(sockfd);
-        exit(EXIT_FAILURE);
-    }
-
-    cout << "Connected to server" << endl;
-    cout << "Enter your identification: ";
-
-    string identification;
-    getline(cin, identification);
-    send(sockfd, identification.c_str(), identification.length(), 0);
-
-    while (true) {
-        cout << "Enter your message: ";
-        string message;
-        getline(cin, message);
-        if (message == "/exit") {
-            send(sockfd, message.c_str(), message.length(), 0);
-            break;
-        }
-        send(sockfd, message.c_str(), message.length(), 0);
-        char buffer[256] = {0};
-        int n = recv(sockfd, buffer, sizeof(buffer) - 1, 0);
-        if (n > 0) {
-            buffer[n] = '\0';
-            cout << "Server: " << buffer << endl;
-        } else {
-            cerr << "Error receiving data from server." << endl;
-            break;
-        }
-    }
-
-    close(sockfd);
     return 0;
 }
--- src/model/server.cpp
+++ src/model/server.cpp
@@ -4,7 +4,110 @@
 #include <cstdlib>
 #include <unistd.h>
 #include <cstring>
+#include <thread>
+#include <mutex>
+
 using namespace std;
+
+class Server {
+public:
+    Server(int port) : port(port), running(true) {
+        setup_server();
+    }
+
+    ~Server() {
+        stop();
+    }
+
+    void run() {
+        while (running) {
+            sockaddr_in client_addr;
+            socklen_t client_addr_len = sizeof(client_addr);
+            int new_sockfd = accept(sockfd, (struct sockaddr*)&client_addr, &client_addr_len);
+            if (new_sockfd < 0) {
+                cerr << "Failed to accept new connection." << endl;
+                continue;
+            }
+            cout << "New client connected." << endl;
+            thread client_thread(&Server::handle_client, this, new_sockfd);
+            client_thread.detach();
+        }
+    }
+
+    void stop() {
+        running = false;
+        close(sockfd);
+    }
+
+private:
+    int port, sockfd;
+    const int BUFFER_SIZE = 256;
+    bool running;
+    mutex mtx;
+
+    void setup_server() {
+        sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        if (sockfd == -1) {
+            cerr << "Failed to create socket." << endl;
+            exit(EXIT_FAILURE);
+        }
+        sockaddr_in sockaddr;
+        sockaddr.sin_family = AF_INET;
+        sockaddr.sin_addr.s_addr = INADDR_ANY;
+        sockaddr.sin_port = htons(port);
+        if (bind(sockfd, (struct sockaddr*)&sockaddr, sizeof(sockaddr)) < 0) {
+            cerr << "Error binding socket... " << endl;
+            close(sockfd);
+            exit(EXIT_FAILURE);
+        }
+        if (listen(sockfd, 10) < 0) {
+            cerr << "Failed to listen on socket." << endl;
+            close(sockfd);
+            exit(EXIT_FAILURE);
+        }
+        cout << "Server is listening on port " << port << " ..." << endl;
+    }
+
+    void handle_client(int client_sockfd) {
+        char buffer[BUFFER_SIZE];
+        int bytesRead;
+        string client_id;
+
+        bytesRead = read(client_sockfd, buffer, BUFFER_SIZE - 1);
+        if (bytesRead > 0) {
+            buffer[bytesRead] = '\0';
+            client_id = buffer;
+            lock_guard<mutex> lock(mtx);
+            cout << "Client identified as: " << client_id << endl;
+        } else {
+            cerr << "Error receiving identification from client." << endl;
+            close(client_sockfd);
+            return;
+        }
+
+        while (running) {
+            memset(buffer, 0, BUFFER_SIZE);
+            bytesRead = read(client_sockfd, buffer, BUFFER_SIZE - 1);
+            if (bytesRead > 0) {
+                buffer[bytesRead] = '\0';
+                string message = buffer;
+                if (message == "/exit") {
+                    lock_guard<mutex> lock(mtx);
+                    cout << "Client " << client_id << " disconnected." << endl;
+                    break;
+                }
+                lock_guard<mutex> lock(mtx);
+                cout << client_id << ": " << message << endl;
+                string response = "message received";
+                send(client_sockfd, response.c_str(), response.size(), 0);
+            } else {
+                cerr << "Error reading from client or client disconnected." << endl;
+                break;
+            }
+        }
+        close(client_sockfd);
+    }
+};
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -17,56 +120,8 @@ int main(int argc, char *argv[]) {
         cerr << "Invalid port number. Port must be between 1 and 65535." << endl;
         exit(EXIT_FAILURE);
     }
+    Server server(port);
+    server.run();
 
-	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd == -1) {
-		cerr << "Failed to create socket." << endl;
-		exit(EXIT_FAILURE);
-	}
-
-	sockaddr_in sockaddr;
-    sockaddr.sin_family = AF_INET;
-    sockaddr.sin_addr.s_addr = INADDR_ANY;
-    sockaddr.sin_port = htons(port);
-
-    if (bind(sockfd, (struct sockaddr*)&sockaddr, sizeof(sockaddr)) < 0) {
-    	cerr << "Error binding socket... " << endl;
-        close(sockfd);
-    	exit(EXIT_FAILURE);
-    }
-
-    if (listen(sockfd, 10) < 0) {
-    	cerr << "Failed to listen on socket." << endl;
-        close(sockfd);
-    	exit(EXIT_FAILURE);
-    }
-    cout << "Server is listening on port " << port << " ..." << endl;
-
-    auto addrlen = sizeof(sockaddr);
-    int connection = accept(sockfd, (struct sockaddr*)&sockaddr, (socklen_t*)&addrlen);
-    if (connection < 0) {
-    	cerr << "Failed to grab connection." << endl;
-        close(sockfd);
-    	exit(EXIT_FAILURE);
-    }
-
-    string client_message;
-    while (true) {
-        char buffer[256] = {0};
-        int bytesRead = read(connection, buffer, sizeof(buffer) - 1);
-        if (bytesRead > 0) {
-            buffer[bytesRead] = '\0';
-            client_message = buffer;
-            cout << "Client: " << client_message << endl;
-            string response = "message received";
-            send(connection, response.c_str(), response.size(), 0);
-        } else {
-            cerr << "Error reading from client or client disconnected." << endl;
-            break;
-        }
-    }
-
-    close(connection);
-    close(sockfd);
     return 0;
 }
