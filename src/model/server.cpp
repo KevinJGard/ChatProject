7e3381e7a1abc12dbbcd58a4a8527fcd7537b435
KevinJGard <kevingard@ciencias.unam.mx>
2024-09-10 01:13:26 -0600

    -Empece con la implementación de JSON junto con el protocolo en el servidor para asi tener una especie de chat global

--- src/model/server.cpp
+++ src/model/server.cpp
@@ -6,6 +6,10 @@
 #include <cstring>
 #include <thread>
 #include <mutex>
+#include <unordered_set>
+#include <unordered_map>
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
 
 using namespace std;
 
@@ -41,9 +45,11 @@ public:
 
 private:
     int port, sockfd;
-    const int BUFFER_SIZE = 256;
+    const int BUFFER_SIZE = 1024;
     bool running;
     mutex mtx;
+    unordered_set<string> usernames;
+    unordered_map<string, int> user_socket_map;
 
     void setup_server() {
         sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -76,8 +82,24 @@ private:
         bytesRead = read(client_sockfd, buffer, BUFFER_SIZE - 1);
         if (bytesRead > 0) {
             buffer[bytesRead] = '\0';
-            client_id = buffer;
+            string message = buffer;
+            json identification = json::parse(message);
+            client_id = identification["username"];
             lock_guard<mutex> lock(mtx);
+            if (usernames.find(client_id) != usernames.end()) {
+                json identification_exits = {
+                    {"type", "RESPONSE"},
+                    {"operation", "IDENTIFY"},
+                    {"result", "USER_ALREADY_EXISTS"},
+                    {"extra", client_id}
+                };
+                string msg = identification_exits.dump();
+                send(client_sockfd, msg.c_str(), msg.length(), 0);
+                close(client_sockfd);
+                return;
+            }
+            usernames.insert(client_id);
+            user_socket_map[client_id] = client_sockfd;
             cout << "Client identified as: " << client_id << endl;
         } else {
             cerr << "Error receiving identification from client." << endl;
@@ -91,21 +113,46 @@ private:
             if (bytesRead > 0) {
                 buffer[bytesRead] = '\0';
                 string message = buffer;
-                if (message == "/exit") {
+                json json_message = json::parse(message);
+                if (json_message["type"] == "PUBLIC_TEXT") {
+                    json public_text = {
+                        {"type", "PUBLIC_TEXT_FROM"},
+                        {"username", client_id},
+                        {"text", json_message["text"]}
+                    };
+                    message_everyone(public_text, client_id);
+                } else if (json_message["type"] == "DISCONNECT") {
                     lock_guard<mutex> lock(mtx);
+                    usernames.erase(client_id);
+                    user_socket_map.erase(client_id);
                     cout << "Client " << client_id << " disconnected." << endl;
                     break;
+                } else {
+                    lock_guard<mutex> lock(mtx);
+                    cout << client_id << ": " << message << endl;
+                    string response = "message received";
+                    send(client_sockfd, response.c_str(), response.size(), 0);
                 }
-                lock_guard<mutex> lock(mtx);
-                cout << client_id << ": " << message << endl;
-                string response = "message received";
-                send(client_sockfd, response.c_str(), response.size(), 0);
             } else {
                 cerr << "Error reading from client or client disconnected." << endl;
                 break;
             }
         }
         close(client_sockfd);
+    }
+
+    void message_everyone(const json& message, const string& user) {
+        string msg = message.dump();
+        lock_guard<mutex> lock(mtx);
+        for (unordered_map<string, int>::iterator it = user_socket_map.begin(); it != user_socket_map.end(); ++it) {
+            const string& username = it->first;
+            int client_sockfd = it->second;
+            if (username != user) {
+                if (send(client_sockfd, msg.c_str(), msg.length(), 0) < 0) {
+                    cerr << "Error sending message to client " << username << endl;
+                }
+            }
+        }
     }
 };
 
